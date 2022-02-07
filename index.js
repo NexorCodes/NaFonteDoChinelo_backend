7b6b21c3c46ae383fc81d0752b34399cb4f2e9cc
nexorgames <97068305+NexorCodes@users.noreply.github.com>
2022-02-07 14:59:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const app = express()
 const database = require('./src/database')
 const router = require('./src/routes')
 
-
-app.use(express.static(__dirname + '/public'))
-app.use(express.urlencoded({ extended: false, limit: '50mb' }))
-app.use(express.json())
-app.use(helmet())
+// app.use(helmet())
 app.use(compression())
 app.use(cors())
+app.use(express.urlencoded({ extended: false, limit: '50mb' }))
+app.use(express.json())
+app.use(express.static(__dirname + '/public'))
 app.use(router)
 
 app.get('*', (request, response) => {
