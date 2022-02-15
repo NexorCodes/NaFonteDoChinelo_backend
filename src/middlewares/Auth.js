bcfe57f8534e06e898b8a7eb041f4d87b14be242
nexorgames <97068305+NexorCodes@users.noreply.github.com>
2022-02-15 13:50:14 -0300

    change auth error response field

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -5,20 +5,20 @@ module.exports = (request, response, next) => {
   const authHeader = request.headers.authorization
 
   if(!authHeader)
-  return response.status(401).json({ ok: false, message: 'No token provided!', notallowed: true })
+  return response.status(401).json({ error: true, message: 'No token provided!', notallowed: true })
 
   const parts = authHeader.split(' ')
 
   if(!parts.length === 2)
-  return response.status(401).json({ ok: false, message: 'Token error!', notallowed: true })
+  return response.status(401).json({ error: true, message: 'Token error!', notallowed: true })
 
   const [ scheme, token ] = parts
 
   if(!scheme === 'Bearer')
-  return response.status(401).json({ ok: false, message: 'Token malformatted!', notallowed: true })
+  return response.status(401).json({ error: true, message: 'Token malformatted!', notallowed: true })
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if(err) return response.status(401).json({ ok:false, messsage: 'Token invalid!', notallowed: true })
+    if(err) return response.status(401).json({ error: true, messsage: 'Token invalid!', notallowed: true })
 
     request.id = decoded.id
     request.email = decoded.email
